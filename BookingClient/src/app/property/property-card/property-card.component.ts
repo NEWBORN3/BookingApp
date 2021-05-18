27dc2fd1a11e5fc72a00bc5ce952514f9b462a53
newBorn <newborn3x@gmail.com>
2021-05-18 14:36:40 +0300

    Client application added

---
+++ BookingClient/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+
+
+@Component ( {
+     selector: 'app-property-card',
+     templateUrl: 'property-card.component.html',
+     styleUrls:  ['property-card.component.css']
+}
+
+)
+export class PropertyCardComponent {
+  Property: any = {
+    "Id": 1,
+    "Name": "DreamWorks",
+    "Type": "House",
+    "Price":120000
+
+  }
+}
